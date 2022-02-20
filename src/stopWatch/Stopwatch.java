140597b46929a3c068953106f10d5769465ccc9b
SidM12345 <90653607+SidM12345@users.noreply.github.com>
2022-02-20 10:06:09 -0700

    Create the stopwatch
    adds a stopwatch

---
+++ src/stopWatch/Stopwatch.java
@@ -0,0 +1,71 @@
+package stopWatch;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Stopwatch implements ActionListener{
+	
+	JFrame frame = new JFrame();
+	JLabel timeLabel = new JLabel();
+	int elapsedTime;
+	int seconds;
+	int minutes;
+	int hours;
+	String sec = String.format("%02d", seconds);
+	String min = String.format("%02d", minutes);
+	String hour = String.format("%02d", hours);
+	
+	Timer timer = new Timer(1000, new ActionListener() {
+		
+		public void actionPerformed (ActionEvent e) {
+			elapsedTime = elapsedTime + 1000;
+			hours = (elapsedTime/3600000);
+			minutes = (elapsedTime/60000)%60;
+			seconds = (elapsedTime/1000)%60;
+			String sec = String.format("%02d", seconds);
+			String min = String.format("%02d", minutes);
+			String hour = String.format("%02d", hours);
+			timeLabel.setText(hour + ":" + min + ":" + sec);
+		}
+	});
+	
+	Stopwatch(){
+		
+		timeLabel.setText(hour + ":" + min + ":" + sec);
+		timeLabel.setBounds(100,100,200,100);
+		timeLabel.setFont(new Font ("Constantia", Font.PLAIN, 35));
+		timeLabel.setBorder(BorderFactory.createBevelBorder(1));
+		timeLabel.setHorizontalAlignment(JTextField.CENTER);
+		frame.add(timeLabel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,420);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+
+	void start() {
+		timer.start();
+	}
+
+	void end() {
+		timer.stop();
+		elapsedTime = 0;
+		
+	}
+	public int getSeconds () {
+		return seconds;
+	}
+	public int getMinutes() {
+		return minutes;
+		
+	}
+	public int getHours() {
+		return hours;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
