32389f49f543416c56a843dd17121d77c21a0c27
Ramya Iyer <ramyaiyer1000@gmail.com>
2022-02-20 14:37:33 -0600

    Create Start.java
    add start menu

---
+++ src/menu/Start.java
@@ -0,0 +1,53 @@
+package menu;
+import java.awt.event.*;
+import input.KeyDetector;
+import snake.Launcher;
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import java.math.*;
+import java.util.*;
+import java.lang.*;
+
+public class Start implements ActionListener {
+
+	JFrame frame;
+	JButton startButton, helpButton, quitButton;
+	JLabel title;
+	public Start() {
+		JFrame frame = new JFrame();
+		title = new JLabel("Quantum Snake Game");
+		startButton = new JButton("Start");
+		startButton.setMargin(new Insets(10,10,10,10));
+		helpButton = new JButton("Help");
+		helpButton.setMargin(new Insets(10,10,10,10));
+		quitButton = new JButton("Quit");
+		quitButton.setMargin(new Insets(10,10,10,10));
+		
+		startButton.addActionListener(this);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(title);
+		panel.add(startButton);
+		panel.add(helpButton);
+		panel.add(quitButton);
+		
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Quantum Snake Game Menu");
+		frame.pack();
+		frame.setVisible(true);
+			
+	}
+
+	public static void main(String[] args) {
+		new Start();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if ()
+	}
+}
